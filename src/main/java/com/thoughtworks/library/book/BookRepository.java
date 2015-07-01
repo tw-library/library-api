dec4bba83f15dfda463deddf7eb2d6b0bdedccf5
tuliolucas <tuliolucas.silva@gmail.com>
2015-07-01 18:19:07 -0300

    Hidding POST, PATCH,DELEFROM of the BookRestService

--- src/main/java/com/thoughtworks/library/book/BookRepository.java
+++ src/main/java/com/thoughtworks/library/book/BookRepository.java
@@ -3,10 +3,20 @@ package com.thoughtworks.library.book;
 import org.jboss.logging.annotations.Param;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.List;
 
-@RepositoryRestResource()
+
+@RepositoryRestResource
 public interface BookRepository extends CrudRepository<Book, Integer>{
+
+    @Override
+    @RestResource(exported = false)
+    public Book save(Book s);
+
+    @Override
+    @RestResource(exported = false)
+    public void delete(Book t);
 
 }
