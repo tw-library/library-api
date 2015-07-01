0810521043332957be671939b53168590fd6d94f
tuliolucas <tuliolucas.silva@gmail.com>
2015-07-01 16:41:20 -0300

    fixing reference about BookRestControllerTest

--- src/test/java/com/thoughtworks/library/BookRestControllerTest.java
+++ src/test/java/com/thoughtworks/library/BookRestControllerTest.java
@@ -92,11 +92,11 @@ public class BookRestControllerTest {
                     .andExpect(jsonPath("$_embedded.books", hasSize(2)))
                     .andExpect(jsonPath("$_embedded.books[0].title", is(book1.getTitle())))
                     .andExpect(jsonPath("$_embedded.books[0].author", is(book1.getAuthor())))
-                    .andExpect(jsonPath("$_embedded.books[0].status", is(book1.getStatus())))
+                    .andExpect(jsonPath("$_embedded.books[0].status", is(book1.getStatus().name())))
 
                     .andExpect(jsonPath("$_embedded.books[1].title", is(book2.getTitle())))
                     .andExpect(jsonPath("$_embedded.books[1].author", is(book2.getAuthor())))
-                    .andExpect(jsonPath("$_embedded.books[1].status", is(book2.getStatus())));
+                    .andExpect(jsonPath("$_embedded.books[1].status", is(book2.getStatus().name())));
 
 
         }catch (Exception e){
