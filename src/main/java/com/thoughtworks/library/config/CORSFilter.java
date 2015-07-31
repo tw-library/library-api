93e2a0d3c7cc0d2e701c90633ee7a4b69ad02680
Boadu Akoto Charles Jnr <ekowcharles@yahoo.co.uk>
2015-07-31 18:29:51 -0300

    removes unused imports

--- src/main/java/com/thoughtworks/library/config/CORSFilter.java
+++ src/main/java/com/thoughtworks/library/config/CORSFilter.java
@@ -2,15 +2,9 @@ package com.thoughtworks.library.config;
 
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import javax.servlet.*;
 import javax.servlet.http.HttpServletResponse;
-import org.springframework.stereotype.Component;
+import java.io.IOException;
 
 /**
  *  CORS: Cross-origin resource sharing: is a mechanism that allows restricted resources (e.g. fonts, JavaScript, etc.)
--- src/main/java/com/thoughtworks/library/exceptions/BookNotAvailableException.java
+++ src/main/java/com/thoughtworks/library/exceptions/BookNotAvailableException.java
@@ -2,7 +2,6 @@ package com.thoughtworks.library.exceptions;
 
 public class BookNotAvailableException extends RuntimeException {
 
-
     public BookNotAvailableException(){
         super("Book requested is not available.");
     }
