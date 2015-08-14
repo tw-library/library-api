b96fd0529fc1e6a2021a2b21708f44bc53611e0b
Raquel Lemos <raqlem@gmail.com>
2015-08-14 17:20:20 -0300

    [#33] Allow Location to be exposed in Header

--- src/main/java/com/thoughtworks/librarysystem/commons/config/CORSFilter.java
+++ src/main/java/com/thoughtworks/librarysystem/commons/config/CORSFilter.java
@@ -20,6 +20,7 @@ public class CORSFilter implements Filter {
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
         response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        response.setHeader("Access-Control-Expose-Headers", "Location");
         chain.doFilter(req, res);
     }
 
