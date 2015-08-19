1e73b4c4db1f1cd1476e53edce96730220cba26d
tuliolucas <tuliolucas.silva@gmail.com>
2015-08-19 17:07:47 -0300

    reflecting changes in copy projection

--- src/main/java/com/thoughtworks/librarysystem/copy/projections/CopyWithBookInline.java
+++ src/main/java/com/thoughtworks/librarysystem/copy/projections/CopyWithBookInline.java
@@ -3,6 +3,7 @@ package com.thoughtworks.librarysystem.copy.projections;
 import com.thoughtworks.librarysystem.book.Book;
 import com.thoughtworks.librarysystem.copy.Copy;
 import com.thoughtworks.librarysystem.copy.CopyStatus;
+import com.thoughtworks.librarysystem.loan.Loan;
 import org.springframework.data.rest.core.config.Projection;
 
 @Projection(name = "copyWithBookInline", types = { Copy.class })
@@ -15,5 +16,7 @@ public interface CopyWithBookInline {
     String getDonator();
 
     Book getBook();
+
+    Loan getLastLoan();
 
 }
