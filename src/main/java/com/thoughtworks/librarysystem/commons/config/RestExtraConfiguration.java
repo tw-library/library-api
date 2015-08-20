68f5a2705255d3e09d9d8e6f8c570f0c318e2b17
Boadu Akoto Charles Jnr <ekowcharles@yahoo.co.uk>
2015-08-20 13:29:46 -0300

    Increases default page size
    @ekowcharles / @raqlem

--- src/main/java/com/thoughtworks/librarysystem/commons/config/RestExtraConfiguration.java
+++ src/main/java/com/thoughtworks/librarysystem/commons/config/RestExtraConfiguration.java
@@ -14,7 +14,7 @@ public class RestExtraConfiguration extends RepositoryRestMvcConfiguration {
     protected void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
         config.exposeIdsFor(Book.class);
         config.exposeIdsFor(Loan.class);
-        config.setMaxPageSize(1000);
+        config.setDefaultPageSize(1000);
     }
 
 
