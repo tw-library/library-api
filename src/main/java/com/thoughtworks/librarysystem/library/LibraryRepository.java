2cdd6c6db4625b3d022e56f2cabf8b241630c286
Boadu Akoto Charles Jnr <ekowcharles@yahoo.co.uk>
2015-08-21 14:15:24 -0300

    [#41] Searches library by slug

--- src/main/java/com/thoughtworks/librarysystem/library/LibraryRepository.java
+++ src/main/java/com/thoughtworks/librarysystem/library/LibraryRepository.java
@@ -1,8 +1,14 @@
 package com.thoughtworks.librarysystem.library;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
 
 @RepositoryRestResource()
 public interface LibraryRepository extends CrudRepository<Library, Integer> {
+
+    List<Library> findBySlug(@Param("slug") String slug);
+
 }
