d7e30cc0c12858f227be64db0abe38ff47d708b5
Dawson Freitas Israel <dawsonfi@gmail.com>
2015-10-02 15:37:33 -0300

    [Dawson|Tulio] Create method to find copies by library slug.

--- src/main/java/com/thoughtworks/librarysystem/copy/CopyRepository.java
+++ src/main/java/com/thoughtworks/librarysystem/copy/CopyRepository.java
@@ -1,10 +1,17 @@
 package com.thoughtworks.librarysystem.copy;
 
 import com.thoughtworks.librarysystem.copy.projections.CopyWithBookInline;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
 
 @RepositoryRestResource(excerptProjection = CopyWithBookInline.class)
 public interface CopyRepository extends PagingAndSortingRepository<Copy, Integer> {
+
+    @Query(name = "select c from Copy c where c.library.slug = :slug")
+    List<Copy> findCopiesByLibrarySlug(@Param("slug") String slug);
+
 }
