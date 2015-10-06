cd36754c0cb3b8aeef3bee44bc6a8df10dea64a2
Dawson Freitas Israel <dawsonfi@gmail.com>
2015-10-06 18:06:45 -0300

    [Dawson|Tulio] Allowed token as request header parameter.

--- src/main/java/com/thoughtworks/librarysystem/commons/config/CORSFilter.java
+++ src/main/java/com/thoughtworks/librarysystem/commons/config/CORSFilter.java
@@ -19,7 +19,7 @@ public class CORSFilter implements Filter {
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PATCH");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
-        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token");
         response.setHeader("Access-Control-Expose-Headers", "Location");
         chain.doFilter(req, res);
     }
