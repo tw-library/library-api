d5a4c5bb41ef912ce93b3d6e525de965e689c674
Francieli Viane <francielifrv@gmail.com>
2016-01-23 00:42:53 -0200

    Refactoring tests

--- src/test/java/com/thoughtworks/librarysystem/commons/EmailValidatorTest.java
+++ src/test/java/com/thoughtworks/librarysystem/commons/EmailValidatorTest.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.librarysystem.commons;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -7,22 +8,29 @@ import static org.hamcrest.core.Is.is;
 
 public class EmailValidatorTest {
 
+    private EmailValidator validator;
+
+    @Before
+    public void setUp() {
+        validator = new EmailValidator();
+
+    }
+
     @Test
-    public void shouldReturnTrueForValidEmail() throws Exception {
-        EmailValidator validator = new EmailValidator();
+    public void shouldReturnTrueForValidEmail() {
         String validEmail = "valid@email.com";
 
         assertThat(validator.validate(validEmail), is(true));
     }
 
     @Test
-    public void shouldReturnFalseForInvalidEmail() throws Exception {
-        EmailValidator validator = new EmailValidator();
+    public void shouldReturnFalseForInvalidEmail() {
         String invalidEmail = "invalid@email";
 
         assertThat(validator.validate(invalidEmail), is(false));
 
         String anotherInvalidEmail = "invalid.com";
+        
         assertThat(validator.validate(anotherInvalidEmail), is(false));
     }
 }
