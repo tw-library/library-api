14df8a8d0d82a6e53ba18c642fc841a9091c8b3e
Francieli Viane <francielifrv@gmail.com>
2016-01-26 11:27:39 -0200

    Asserts that email will be got from loan when loan object has email information

--- src/test/java/com/thoughtworks/librarysystem/loan/LoanTest.java
+++ src/test/java/com/thoughtworks/librarysystem/loan/LoanTest.java
@@ -2,12 +2,20 @@ package com.thoughtworks.librarysystem.loan;
 
 import com.thoughtworks.librarysystem.copy.Copy;
 import com.thoughtworks.librarysystem.user.User;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.assertThat;
 
 public class LoanTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+
+    }
 
     @Test
     public void shouldGetEmailAddressFromUserObjectWhenLoanHasNoEmail() throws Exception {
@@ -25,4 +33,21 @@ public class LoanTest {
         assertThat(loan.getEmail(), is(user.getEmail()));
     }
 
+    @Test
+    public void shouldGetEmailAddressFromLoanObjectWhenLoanHasEmail() throws Exception {
+        String userEmail = "user@email.com";
+        User user = new User();
+        user.setEmail(userEmail);
+
+        Copy aCopy = new Copy();
+        aCopy.setId(1);
+
+        Loan loan = new Loan();
+        loan.setCopy(aCopy);
+        loan.setUser(user);
+        loan.setEmail("loan@email.com");
+
+        assertThat(loan.getEmail(), is(not(user.getEmail())));
+        assertThat(loan.getEmail(), is("loan@email.com"));
+    }
 }
