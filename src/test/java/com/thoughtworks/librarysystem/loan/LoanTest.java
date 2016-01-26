7a6dce2cd8d75994b78c0bd407201cbc97e559d9
Francieli Viane <francielifrv@gmail.com>
2016-01-26 11:32:02 -0200

    Refactor tests

--- src/test/java/com/thoughtworks/librarysystem/loan/LoanTest.java
+++ src/test/java/com/thoughtworks/librarysystem/loan/LoanTest.java
@@ -10,42 +10,38 @@ import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.assertThat;
 
 public class LoanTest {
+    User user;
+    Copy aCopy;
+    Loan loan;
+
+    private final String USER_EMAIL = "user@email.com";
 
     @Before
     public void setUp() throws Exception {
 
+        user = new User();
+        user.setEmail(USER_EMAIL);
+
+        aCopy = new Copy();
+        aCopy.setId(1);
+
+        loan = new Loan();
+        loan.setCopy(aCopy);
+        loan.setUser(user);
 
     }
 
     @Test
     public void shouldGetEmailAddressFromUserObjectWhenLoanHasNoEmail() throws Exception {
-        String userEmail = "user@email.com";
-        User user = new User();
-        user.setEmail(userEmail);
-
-        Copy aCopy = new Copy();
-        aCopy.setId(1);
-
-        Loan loan = new Loan();
-        loan.setCopy(aCopy);
-        loan.setUser(user);
 
         assertThat(loan.getEmail(), is(user.getEmail()));
     }
 
     @Test
     public void shouldGetEmailAddressFromLoanObjectWhenLoanHasEmail() throws Exception {
-        String userEmail = "user@email.com";
-        User user = new User();
-        user.setEmail(userEmail);
+        String loanEmail = "loan@email.com";
 
-        Copy aCopy = new Copy();
-        aCopy.setId(1);
-
-        Loan loan = new Loan();
-        loan.setCopy(aCopy);
-        loan.setUser(user);
-        loan.setEmail("loan@email.com");
+        loan.setEmail(loanEmail);
 
         assertThat(loan.getEmail(), is(not(user.getEmail())));
         assertThat(loan.getEmail(), is("loan@email.com"));
