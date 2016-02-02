d292ae79a3b9fae1e9675a25ddd38168a6d6ed7f
eferreir <elayne@gmail.com>
2016-02-02 12:50:36 -0200

    Adding service for copy quantity value

--- src/main/java/com/thoughtworks/librarysystem/copy/CopyRepository.java
+++ src/main/java/com/thoughtworks/librarysystem/copy/CopyRepository.java
@@ -1,6 +1,8 @@
 package com.thoughtworks.librarysystem.copy;
 
 import com.thoughtworks.librarysystem.copy.projections.CopyWithBookInline;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Projections;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
@@ -10,8 +12,11 @@ import java.util.List;
 
 @RepositoryRestResource(excerptProjection = CopyWithBookInline.class)
 public interface CopyRepository extends PagingAndSortingRepository<Copy, Integer> {
-
+    
     @Query(name = "select c from Copy c where c.library.slug = :slug")
     List<Copy> findCopiesByLibrarySlug(@Param("slug") String slug);
+
+    @Query(name = "select count(Copy.copy_id) from Copy where Copy.library.slug = :slug and Copy.book_id= :book")
+    Long countByLibrarySlugAndBookId(@Param("slug") String slug, @Param("book") Integer book);
 
 }
