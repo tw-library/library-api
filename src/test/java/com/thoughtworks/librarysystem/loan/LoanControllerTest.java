66e8980144848eff50d31595e463daa2020238d5
Francieli Viane <francielifrv@gmail.com>
2016-02-01 23:26:04 -0200

    Asserts that http error 409 is will be thrown when copy is not available

--- src/test/java/com/thoughtworks/librarysystem/loan/LoanControllerTest.java
+++ src/test/java/com/thoughtworks/librarysystem/loan/LoanControllerTest.java
@@ -3,7 +3,9 @@ package com.thoughtworks.librarysystem.loan;
 import com.thoughtworks.librarysystem.book.Book;
 import com.thoughtworks.librarysystem.copy.Copy;
 import com.thoughtworks.librarysystem.copy.CopyBuilder;
+import com.thoughtworks.librarysystem.loan.exceptions.CopyIsNotAvailableException;
 import com.thoughtworks.librarysystem.user.User;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -15,8 +17,7 @@ import org.springframework.validation.BindingResult;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class LoanControllerTest {
@@ -27,30 +28,49 @@ public class LoanControllerTest {
     @Mock
     LoanService loanService;
 
-    @Test
-    public void shouldReturnHttpStatusCreatedWhenBookIsBorrowed() throws Exception {
-        User user = new User();
+    private User user;
+    private Book book;
+    private Copy copy;
+    private Loan loan;
+    BindingResult bindingResult;
+
+    @Before
+    public void setUp() throws Exception {
+        user = new User();
         user.setEmail("some@email.com");
 
-        Book book = new Book();
+        book = new Book();
 
-        Copy copy = new CopyBuilder()
+        copy = new CopyBuilder()
                 .withBook(book)
                 .build();
 
-        Loan loan = new LoanBuilder()
+        loan = new LoanBuilder()
                 .withUser(user)
                 .withCopy(copy)
                 .withId(1)
                 .build();
 
-        BindingResult bindingResult = mock(BindingResult.class);
+        bindingResult = mock(BindingResult.class);
+    }
 
+    @Test
+    public void shouldReturnHttpStatusCreatedWhenBookIsBorrowed() throws Exception {
         when(loanService.borrowCopy(copy.getId(), user.getEmail())).thenReturn(loan);
-        ResponseEntity currentResponse = controller.borrowBook(loan, bindingResult);
 
+        ResponseEntity currentResponse = controller.borrowBook(loan, bindingResult);
         ResponseEntity expectedResponse = new ResponseEntity(HttpStatus.CREATED);
 
         assertThat(currentResponse, is(expectedResponse));
+    }
+
+    @Test
+    public void shouldReturnHttpConflictStatusWhenBookIsNotAvailable() throws Exception {
+        doThrow(new CopyIsNotAvailableException()).when(loanService).borrowCopy(copy.getId(), user.getEmail());
+
+        ResponseEntity currentResponse = controller.borrowBook(loan, bindingResult);
+        ResponseEntity expectedResponse = new ResponseEntity(HttpStatus.CONFLICT);
+
+        assertThat(currentResponse.getStatusCode(), is(expectedResponse.getStatusCode()));
     }
 }
