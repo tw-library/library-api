58422d4f988c99ef5694ede8e0e155b313817271
eferreir <elayne@gmail.com>
2016-02-03 11:32:16 -0200

    Story 101 - Adding service for book availability

--- src/main/java/com/thoughtworks/librarysystem/copy/CopyRepository.java
+++ src/main/java/com/thoughtworks/librarysystem/copy/CopyRepository.java
@@ -1,8 +1,6 @@
 package com.thoughtworks.librarysystem.copy;
 
 import com.thoughtworks.librarysystem.copy.projections.CopyWithBookInline;
-import org.hibernate.Criteria;
-import org.hibernate.criterion.Projections;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
@@ -12,11 +10,15 @@ import java.util.List;
 
 @RepositoryRestResource(excerptProjection = CopyWithBookInline.class)
 public interface CopyRepository extends PagingAndSortingRepository<Copy, Integer> {
-    
-    @Query(name = "select c from Copy c where c.library.slug = :slug")
+
+    @Query(name = "select c from Copy c where c.library.slug = :slug limit 1")
     List<Copy> findCopiesByLibrarySlug(@Param("slug") String slug);
 
     @Query(name = "select count(Copy.copy_id) from Copy where Copy.library.slug = :slug and Copy.book_id= :book")
     Long countByLibrarySlugAndBookId(@Param("slug") String slug, @Param("book") Integer book);
+
+
+    @Query(name = "select count(Copy.copy_id) from Copy where Copy.library.slug = :slug and Copy.book_id= :book and status = AVAILABLE")
+    Long countByLibrarySlugAndBookIdAndStatus(@Param("slug") String slug, @Param("book") Integer book, @Param("status") CopyStatus status);
 
 }
