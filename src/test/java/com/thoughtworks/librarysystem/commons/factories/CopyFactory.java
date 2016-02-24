109b6c108aa167d386b60a05cb22ea25b08f9f55
eferreir <elayne@gmail.com>
2016-02-24 14:40:10 -0300

    Adding resources factory to improve tests

---
+++ src/test/java/com/thoughtworks/librarysystem/commons/factories/CopyFactory.java
@@ -0,0 +1,48 @@
+package com.thoughtworks.librarysystem.commons.factories;
+
+
+import com.thoughtworks.librarysystem.book.Book;
+import com.thoughtworks.librarysystem.copy.Copy;
+import com.thoughtworks.librarysystem.copy.CopyBuilder;
+import com.thoughtworks.librarysystem.library.Library;
+
+/**
+ * Created by eferreir on 2/8/16.
+ */
+public class CopyFactory {
+
+
+    public Copy createStandardCopyWithSameIsbnAndLibrary() {
+
+        Book book = new BookFactory().createBookWithStandardIsbn();
+        Library library = new LibraryFactory().createStardardLibrary();
+
+        Copy copy = new CopyBuilder()
+                .withBook(book)
+                .withLibrary(library)
+                .build();
+        return copy;
+    }
+
+
+    public Copy createStandardCopyWithSpecificLibrary(Library library) {
+        Book book = new BookFactory().createBookWithStandardIsbn();
+
+        Copy copy = new CopyBuilder()
+                .withBook(book)
+                .withLibrary(library)
+                .build();
+        return copy;
+    }
+
+
+    public Copy createCopyWithLibraryAndBook(Library library, Book book) {
+
+        Copy copy = new CopyBuilder()
+                .withBook(book)
+                .withLibrary(library)
+                .build();
+        return copy;
+    }
+
+}
